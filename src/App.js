c3bbd6a0aaad3f4d7506279c226477d037be8900
Eshwar-c <eshwar1652@gmail.com>
2024-03-11 20:18:16 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import './stylesheet.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className='nav-bar'>
+        <text className='text'>QA space</text>
+        <button className='home'>Home</button>
+        <button className='about'>About us</button>
+      </nav>
+      <div className="center-content">
+      <h1>Search Answers for MCQ questions</h1>
+      <input type="text" placeholder='search here'></input>
+      <button placeholder='search'></button>
+      <button placeholder='post q&a'></button>
     </div>
+    </div>
+    
   );
 }
 
